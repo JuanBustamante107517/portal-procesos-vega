9ca910594c42d3eecfe0ddfecde2de199bb1e5da
JuanBustamante107517 <juan.bustamante.g@tecsup.edu.pe>
2025-12-11 20:40:40 -0500

    feat: Implementa Docker y Docker Compose para Backend y Frontend

--- frontend/postcss.config.js
+++
@@ -1,9 +0,0 @@
-// frontend/postcss.config.js
-export default {
-  plugins: {
-    // ANTES decía: 'tailwindcss': {},
-    // AHORA usamos el nuevo paquete:
-    '@tailwindcss/postcss': {}, 
-    'autoprefixer': {},
-  },
-}
